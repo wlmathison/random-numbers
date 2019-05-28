dc93ca636952b1ff0f4a5501c5b6a6b31db298ce
Billy Mathison <wlmathison@gmail.com>
2019-05-28 11:53:25 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace random_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
